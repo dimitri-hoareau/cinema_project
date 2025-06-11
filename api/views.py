cfb7fa8ce0343488d63d3d0c2e0b06d6792cff1b
dimitri-hoareau <dimitrihoareau2@gmail.com>
2025-06-11 07:26:03 +0200

    Deleter an author only if he had no movies

--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets,  mixins, status
+from rest_framework.response import Response
 from core.models import Author
 from .serializers import AuthorSerializer
 
@@ -13,3 +14,13 @@ class AuthorViewSet(mixins.ListModelMixin,
 
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        author = self.get_object()
+        if author.films.count() > 0:
+            return Response(
+                {'error': "Can't delete an author who have a movie"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        return super().destroy(request, *args, **kwargs)
+
